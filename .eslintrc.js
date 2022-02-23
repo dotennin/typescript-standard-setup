d9cb2e64cada94f07ef53fdbad8fbb3411971cff
Dotennin <wenhua4438@gmail.com>
2022-02-23 23:44:03 +0900

    chore: use string type rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,23 +30,23 @@ module.exports = {
       }
     ],
     "@typescript-eslint/adjacent-overload-signatures": "error",
-    "@typescript-eslint/explicit-function-return-type": 0,
-    "@typescript-eslint/member-delimiter-style": 0,
-    "@typescript-eslint/interface-name-prefix": 0,
-    "@typescript-eslint/no-non-null-assertion": 0,
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/member-delimiter-style": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "no-console": "off",
     'indent': 'off',
     '@typescript-eslint/indent': 'off',
-    "eqeqeq": 2,
-    "no-var": 2,
-    "prefer-const": 2,
+    "eqeqeq": "error",
+    "no-var": "error",
+    "prefer-const": "error",
   },
   overrides: [
     {
       files: ["*.js"],
       rules: {
-        "@typescript-eslint/no-var-requires": 0
+        "@typescript-eslint/no-var-requires": "off"
       }
     }
   ]
